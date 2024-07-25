9931ba14c78ee183410c047cb25a2ff32fb0ef23
Lilhorenikita <nikitapalnesto19@gmail.com>
2024-07-25 18:38:35 +0530

    feat:server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const route = require("./Route/route");
-const { default: mongoose } = require("mongoose");
-const { Route } = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
-const app = express();
 const multer = require("multer");
 const cookieSession = require("cookie-session");
+require('dotenv').config();
+
+const app = express();
+
 // Enable All CORS Requests for development use
-
-
 app.use(
   cors({
     origin: [
@@ -17,8 +17,7 @@ app.use(
       "http://localhost:444",
       "http://localhost:5174",
       "http://localhost:3000",
-      "http://localhost:80",
-
+      "http://localhost:80"
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
@@ -27,32 +26,34 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization", "Origin", "Cookie"],
   })
 );
+
 app.use(multer().any());
-require('dotenv').config()
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 mongoose
   .connect(
     "mongodb+srv://dxv42198:KVLcA1bWcl3KXmuZ@cluster0.qnt5rum.mongodb.net/ExportITMain",
-    // "mongodb+srv://qjoxqciedfjvrzyeyh:oVDaqdgLGKDxYT58@cluster0.kczadan.mongodb.net/ExportIT",
     {
       useNewUrlParser: true,
     }
   )
   .then(() => console.log("MongoDb is connected"))
   .catch((err) => console.log(err));
+
 app.use(
   cookieSession({
     signed: false,
-
     secure: false,
-
     sameSite: "strict",
   })
 );
+
 app.use("/", route);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Express app running on port " + (process.env.PORT || 3000));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Express app running on port ${PORT}`);
 });
+
+module.exports = app;
